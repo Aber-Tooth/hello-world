40a17be1810e379e150aa62a1c3bb2db40d2a9a9
Aber-Tooth <122560097+Aber-Tooth@users.noreply.github.com>
2023-01-13 13:29:59 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Sad)
+    music.ringTone(165)
+    basic.pause(1000)
+    music.stopMelody(MelodyStopOptions.All)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Happy)
+    music.ringTone(659)
+    basic.pause(1000)
+    music.stopMelody(MelodyStopOptions.All)
 })
 basic.showIcon(IconNames.Asleep)
